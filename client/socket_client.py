f5bab9d3e43a58e0e8c5255da0386fe9c4875484
emauric <mauricesingley.ms@gmail.com>
2025-05-30 18:37:29 -0400

    feat: implement basic threaded chat client with read/write

--- client/socket_client.py
+++ client/socket_client.py
@@ -1,1 +1,38 @@
-pass
+import socket
+import sys
+import threading
+
+# Host & Port
+host, port = sys.argv[1], int(sys.argv[2])
+# IPV4 addr, TCP socket type
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(host, port)  # connect to server
+
+
+# send messages via input
+def write_sockets():
+    while True:
+        try:
+            message = input("> ")
+            server.send(message.encode('utf-8'))
+        except:
+            print("An error occurred while sending.")
+            server.close()
+            break
+
+
+# receive messages via recv
+def read_sockets():
+    while True:
+        try:
+            message = server.recv(2048).decode('utf-8')
+            print(message)
+        except:
+            print("An error occurred while reading.")
+            server.close()
+            break
+
+
+# Daemon threads will exit when the main program exits
+t1 = threading.Thread(target=read_sockets, daemon=True).start()
+t2 = threading.Thread(target=write_sockets, daemon=True).start()
